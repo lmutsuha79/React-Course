a260f080b927bb530646c3042801d2216914cdbd
LMUT SUHA <74467521+lmutsuha79@users.noreply.github.com>
2024-12-20 11:49:48 +0100

    Create AnimeData.js

---
+++ AnimeData.js
@@ -0,0 +1,62 @@
+  const animes = [
+    {
+      title: "Fullmetal Alchemist: Brotherhood",
+      description:
+        "Two brothers use alchemy in their quest to restore their bodies after a failed transmutation.",
+      image: "https://cdn.myanimelist.net/images/anime/1223/96541.jpg",
+    },
+    {
+      title: "Attack on Titan",
+      description:
+        "Humanity fights for survival against giant humanoid Titans.",
+      image: "https://cdn.myanimelist.net/images/anime/10/47347.jpg",
+    },
+    {
+      title: "Death Note",
+      description:
+        "A high school student discovers a notebook that allows him to kill anyone by writing their name.",
+      image: "https://cdn.myanimelist.net/images/anime/9/9453.jpg",
+    },
+    {
+      title: "Naruto",
+      description:
+        "A young ninja strives to become the strongest in his village.",
+      image: "https://cdn.myanimelist.net/images/anime/13/17405.jpg",
+    },
+    {
+      title: "One Piece",
+      description:
+        "A pirate and his crew search for the ultimate treasure known as 'One Piece'.",
+      image: "https://cdn.myanimelist.net/images/anime/6/73245.jpg",
+    },
+    {
+      title: "Demon Slayer: Kimetsu no Yaiba",
+      description:
+        "A boy becomes a demon slayer to avenge his family and cure his sister.",
+      image: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg",
+    },
+    {
+      title: "My Hero Academia",
+      description:
+        "In a world where nearly everyone has superpowers, a boy without them still dreams of becoming a hero.",
+      image: "https://cdn.myanimelist.net/images/anime/10/78745.jpg",
+    },
+    {
+      title: "Steins;Gate",
+      description:
+        "A group of friends accidentally invent a method of time travel, leading to unforeseen consequences.",
+      image: "https://cdn.myanimelist.net/images/anime/5/73199.jpg",
+    },
+    {
+      title: "Cowboy Bebop",
+      description:
+        "A group of bounty hunters travel through space in search of their next payday.",
+      image: "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
+    },
+    {
+      title: "Sword Art Online",
+      description:
+        "Players of a virtual reality MMORPG find themselves trapped in the game, with death in-game meaning death in real life.",
+      image: "https://cdn.myanimelist.net/images/anime/11/39717.jpg",
+    },
+  ];
